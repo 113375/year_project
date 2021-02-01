ac0e8df0a2dd265653933c39e9eca6f0182699a3
Дима <knyazevdima05@gmail.com>
2021-02-01 19:08:29 +0300

    Обрвботка запроса processing.php

--- processing.php
+++ processing.php
@@ -10,12 +10,10 @@ if ($_GET['mode'] == 'check' and $_GET['card']) {
 }
 function check_card($card)
 {
-    #проверяет код и сверяет его с базой данных
     $dsn = 'mysql:dbname=school;host=localhost';
     $user = 'root';
     $password = 'root';
     $pdo = new PDO($dsn, $user, $password);
-    #Запрос
 
     $sth = $pdo->prepare("SELECT * FROM students WHERE card_id = :card");
     $sth->bindValue(':card', $card);
